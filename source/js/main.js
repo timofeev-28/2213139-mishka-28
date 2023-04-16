e5d75af5676477b96a45d850c2dc06af64951d06
Dmitrii Timofeev <28dngn@gmail.ru>
2023-04-16 15:31:34 +0300

    бургер-меню дополнение

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,19 @@
 // BURGER-MENU
 
-// let burger = document.querySelector('.burger');
-// let menu = document.querySelector('.navigation');
-// let menuLinks = menu.querySelectorAll('.navigation__link');
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.navigation');
+let menuLinks = menu.querySelectorAll('.navigation__link');
 
-// menu.classList.add('js-active');
+menu.classList.add('js-active');
 
-// burger.addEventListener('click', function () {
+burger.addEventListener('click', function () {
 
-//   menu.classList.toggle('navigation--active');
-// })
+  menu.classList.toggle('navigation--active');
+})
 
-// menuLinks.forEach(function (el) {
-//   el.addEventListener('click', function () {
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
 
-//     menu.classList.remove('navigation--active');
-//   })
-// })
+    menu.classList.remove('navigation--active');
+  })
+})
