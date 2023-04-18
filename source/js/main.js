e0cbbb4c8639968aa272f617e25102de23fa842e
Dmitrii Timofeev <28dngn@gmail.ru>
2023-04-19 01:41:31 +0300

    дом_5 form

--- source/js/main.js
+++ source/js/main.js
@@ -3,16 +3,15 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.navigation');
 let menuLinks = menu.querySelectorAll('.navigation__link');
-let body = document.querySelector('.page-body');
 
+// когда JS есть, .js-active прячет бургер-меню
 menu.classList.add('js-active');
 
 burger.addEventListener('click', function () {
 
   menu.classList.toggle('navigation--active');
   burger.classList.toggle('burger--active');
-  body.classList.toggle('disable-scroll');
-})
+});
 
 menuLinks.forEach(function (el) {
   el.addEventListener('click', function (event) {
@@ -20,4 +19,4 @@ menuLinks.forEach(function (el) {
     menu.classList.remove('navigation--active');
     burger.classList.remove('burger--active');
   })
-})
+});
