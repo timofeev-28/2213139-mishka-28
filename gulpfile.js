f56d02d28521072113fe90ffe8fd4e4269ff3dcf
Dmitrii Timofeev <28dngn@gmail.ru>
2023-05-06 18:50:15 +0300

    ховер-эффекты шапка и подвал

--- gulpfile.js
+++ gulpfile.js
@@ -126,7 +126,7 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles, reload));
   gulp.watch('source/js/script.js', gulp.series(scripts));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
